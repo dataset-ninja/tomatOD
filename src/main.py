c471016a648f3ebeb69315f4a62d28958d13f073
Ars Vladimirov <grokhael@gmail.com>
2023-06-13 00:49:56 +0400

    refactor

--- src/main.py
+++ src/main.py
@@ -1,182 +1,73 @@
+import argparse
 import json
 import os
+import sys
 
+import supervisely as sly
+from dataset_tools import ProjectRepo
 from dotenv import load_dotenv
 
-import dataset_tools as dtools
-import supervisely as sly
+import src.settings as s
+from src.convert import convert_and_upload_supervisely_project
 
-if sly.is_development():
-    load_dotenv(os.path.expanduser("~/ninja.env"))
-    load_dotenv("local.env")
-
-os.makedirs("./stats/", exist_ok=True)
-os.makedirs("./visualizations/", exist_ok=True)
-api = sly.Api.from_env()
-team_id = sly.env.team_id()
-workspace_id = sly.env.workspace_id()
+PARENT_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+LOCAL_ENV = os.path.join(PARENT_PATH, "local.env")
+load_dotenv(os.path.expanduser("~/ninja.env"))
+load_dotenv(LOCAL_ENV)
+SERVER_ADDRESS = os.getenv("SERVER_ADDRESS")
+TEAM_ID = sly.env.team_id()
+WORKSPACE_ID = sly.env.workspace_id()
 
 
-# 1a initialize sly api way
-project_id = sly.env.project_id()
-project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project_id))
-datasets = api.dataset.get_list(project_id)
+def get_project_info(api: sly.Api):
+    s.check_names()
 
+    project_info = api.project.get_info_by_name(WORKSPACE_ID, s.PROJECT_NAME)
+    if not project_info:
+        # If project doesn't found on instance, create it and use new project info.
+        project_info = convert_and_upload_supervisely_project(api, WORKSPACE_ID, s.PROJECT_NAME)
+        sly.logger.info(f"Project {s.PROJECT_NAME} not found on instance. Created new project.")
+        sly.logger.info("Now you can explore created project and choose 'preview_image_id'.")
+        sys.exit(0)
+    else:
+        sly.logger.info(f"Found project {s.PROJECT_NAME} on instance, will use it.")
 
-# 1b initialize sly localdir way
-# project_path = os.environ["LOCAL_DATA_DIR"]
-# sly.download(api, project_id, project_path, save_image_info=True, save_images=False)
-# project_meta = sly.Project(project_path, sly.OpenMode.READ).meta
-# datasets = None
-
-
-project_info = api.project.get_info_by_id(project_id)
-custom_data = project_info.custom_data
-
-# 2. get download link
-download_sly_url = dtools.prepare_download_link(project_info)
-dtools.update_sly_url_dict({project_id: download_sly_url})
-
-
-# 3. upload custom data
-# if len(custom_data) > 0:
-# preset fields
-custom_data = {
-    # required fields
-    "name": "tomatOD",
-    "fullname": "Tomat Object Detection",
-    "cv_tasks": [
-        "object detection",
-    ],
-    "annotation_types": ["object detection"],
-    "industries": ["agriculture"],
-    "release_year": 2020,
-    "homepage_url": "https://github.com/up2metric/tomatOD",
-    "license": "Attribution-NonCommercial-ShareAlike 4.0 International",
-    "license_url": "https://creativecommons.org/licenses/by-nc-sa/4.0/",
-    "preview_image_id": 213786,
-    "github": "dataset-ninja/tomatOD",
-    "github_url": "https://github.com/dataset-ninja/tomatOD",
-    "download_sly_url": download_sly_url,
-    # optional fields
-    "download_original_url": "https://github.com/up2metric/tomatOD#download-the-data",
-    # "paper": "https://ieeexplore.ieee.org/document/8954630",
-    # "citation_url": "https://conservancy.umn.edu/handle/11299/206575",
-    "organization_name": "up2metric",
-    "organization_url": "http://www.up2metric.com/computer-vision-ai/",
-    # "tags": [],
-}
-api.project.update_custom_data(project_id, custom_data)
-
-
-def build_stats():
-    stats = [
-        dtools.ClassBalance(project_meta),
-        dtools.ClassCooccurrence(project_meta, force=False),
-        dtools.ClassesPerImage(project_meta, datasets),
-        dtools.ObjectsDistribution(project_meta),
-        dtools.ObjectSizes(project_meta),
-        dtools.ClassSizes(project_meta),
-    ]
-    heatmaps = dtools.ClassesHeatmaps(project_meta)
-    classes_previews = dtools.ClassesPreview(project_meta, project_info.name, force=False)
-    previews = dtools.Previews(project_id, project_meta, api, team_id)
-
-    for stat in stats:
-        if not sly.fs.file_exists(f"./stats/{stat.basename_stem}.json"):
-            stat.force = True
-    stats = [stat for stat in stats if stat.force]
-
-    if not sly.fs.file_exists(f"./stats/{heatmaps.basename_stem}.png"):
-        heatmaps.force = True
-    if not sly.fs.file_exists(f"./visualizations/{classes_previews.basename_stem}.webm"):
-        classes_previews.force = True
-    if not api.file.dir_exists(team_id, f"/dataset/{project_id}/renders/"):
-        previews.force = True
-    vstats = [stat for stat in [heatmaps, classes_previews, previews] if stat.force]
-
-    dtools.count_stats(
-        project_id,
-        stats=stats + vstats,
-        sample_rate=1,
-    )
-
-    print("Saving stats...")
-    for stat in stats:
-        with open(f"./stats/{stat.basename_stem}.json", "w") as f:
-            json.dump(stat.to_json(), f)
-        stat.to_image(f"./stats/{stat.basename_stem}.png")
-
-    if len(vstats) > 0:
-        if heatmaps.force:
-            heatmaps.to_image(f"./stats/{heatmaps.basename_stem}.png")
-        if classes_previews.force:
-            classes_previews.animate(f"./visualizations/{classes_previews.basename_stem}.webm")
-        if previews.force:
-            previews.close()
-
-    print("Stats done")
-
-
-def build_visualizations():
-    renderers = [
-        dtools.Poster(project_id, project_meta, force=False),
-        dtools.SideAnnotationsGrid(project_id, project_meta),
-    ]
-    animators = [
-        dtools.HorizontalGrid(project_id, project_meta),
-        dtools.VerticalGrid(project_id, project_meta, force=False),
-    ]
-
-    for vis in renderers + animators:
-        if not sly.fs.file_exists(f"./visualizations/{vis.basename_stem}.png"):
-            vis.force = True
-    renderers, animators = [r for r in renderers if r.force], [a for a in animators if a.force]
-
-    for a in animators:
-        if not sly.fs.file_exists(f"./visualizations/{a.basename_stem}.webm"):
-            a.force = True
-    animators = [a for a in animators if a.force]
-
-    # Download fonts from https://fonts.google.com/specimen/Fira+Sans
-    dtools.prepare_renders(
-        project_id,
-        renderers=renderers + animators,
-        sample_cnt=40,
-    )
-    print("Saving visualization results...")
-    for vis in renderers + animators:
-        vis.to_image(f"./visualizations/{vis.basename_stem}.png")
-    for a in animators:
-        a.animate(f"./visualizations/{a.basename_stem}.webm")
-    print("Visualizations done")
-
-
-def build_summary():
-    print("Building summary...")
-    summary_data = dtools.get_summary_data_sly(project_info)
-
-    if sly.fs.file_exists("./visualizations/classes_preview.webm"):
-        classes_preview = (
-            f"{custom_data['github_url']}/raw/main/visualizations/classes_preview.webm"
-        )
-
-    summary_content = dtools.generate_summary_content(
-        summary_data,
-        vis_url=classes_preview,
-    )
-
-    with open("SUMMARY.md", "w") as summary_file:
-        summary_file.write(summary_content)
-    print("Done.")
-
-
-def main():
-    pass
-    build_stats()
-    build_visualizations()
-    build_summary()
+    return project_info
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Upload dataset to instance.")
+    parser.add_argument(
+        "--forces", type=json.loads, default="{}", help="Which parameters to force."
+    )
+
+    args = parser.parse_args()
+    forces = args.forces
+
+    sly.logger.info(f"Script is starting with forces: {forces}")
+
+    sly.fs.mkdir("./stats")
+    sly.fs.mkdir("./visualizations")
+
+    api = sly.Api.from_env()
+    sly.logger.info(
+        f"Connected to Supervisely. Server address: {SERVER_ADDRESS}, team_id: {TEAM_ID}, workspace_id: {WORKSPACE_ID}."
+    )
+    project_id = get_project_info(api).id
+    settings = s.get_settings()
+
+    sly.logger.info(f"Starting to work with project id: {project_id}.")
+
+    force_stats = forces.get("force_stats")
+    force_visuals = forces.get("force_visuals")
+    force_texts = forces.get("force_texts")
+
+    project_repo = ProjectRepo(api, project_id, settings)
+    project_repo.build_stats(force=force_stats)
+    project_repo.build_visualizations(force=force_visuals)
+
+    # * Optional parameter preview_class should be passed if needed:
+    # * Literal["ClassesPreview", "HorizontalGrid", "SideAnnotationsGrid"]
+    project_repo.build_texts(force=force_texts)
+
+    sly.logger.info("Script finished.")
--- src/sly_globals.py
+++ src/sly_globals.py
@@ -36,7 +36,7 @@ test_percent = 100
 
 sample_img_count = {"Train": round(2.22 * train_percent), "Test": round(0.55 * test_percent)}
 
-project_name = "TomatoOD"
+# project_name = "TomatoOD"
 work_dir = "tomato_data"
 images_url = "https://datasets-u2m.s3.eu-west-3.amazonaws.com/tomatOD_images.zip"
 annotations_url = "https://datasets-u2m.s3.eu-west-3.amazonaws.com/tomatOD_annotations.zip"
@@ -63,7 +63,8 @@ tag_metas = [tag_meta_unripe, tag_meta_semi_ripe, tag_meta_fully_ripe]
 tag_meta_collection = sly.TagMetaCollection(tag_metas)
 meta = sly.ProjectMeta(obj_classes=obj_class_collection, tag_metas=tag_meta_collection)
 
-storage_dir = sly.app.get_data_dir()
+# storage_dir = sly.app.get_data_dir()
+storage_dir = "./APP_DATA"
 work_dir_path = os.path.join(storage_dir, work_dir)
 sly.io.fs.mkdir(work_dir_path)
 images_archive_path = os.path.join(work_dir_path, images_arch_name)
